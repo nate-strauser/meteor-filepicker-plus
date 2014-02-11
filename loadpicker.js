1cc146ec329c7e67db7114b87565e836d19b7316
wulx <gurdy.woo@gmail.com>
2014-02-11 15:52:31 +0800

    revise loadpicker

--- loadpicker.js
+++ loadpicker.js
@@ -1,27 +1,26 @@
 loadPicker = function(key, callback) {
-  //Functions to run after the script tag has loaded
-  var filepickerLoadCallback = function(){
-      filepicker.setKey(key);
+  // Functions to run after the script tag has loaded
+  var filepickerLoadCallback = function() {
+    filepicker.setKey(key);
 
-      if (typeof callback !== 'undefined' && callback !== null)
-          callback();
+    if (Object.prototype.toString.call(callback) === "[object Function")
+      callback();
   };
 
-  //If the script doesn't load
-  var filepickerErrorCallback = function(error){
-      if(typeof console != undefined) {
-          console.log(error);
-      }
+  // If the script doesn't load
+  var filepickerErrorCallback = function(error) {
+    if(typeof console !== "undefined") {
+      console.log(error);
+    }
   };
 
-  //Generate a script tag
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = '//api.filepicker.io/v1/filepicker.js';
+  // Generate a script tag
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "//api.filepicker.io/v1/filepicker.js";
   script.onload = filepickerLoadCallback;
   script.onerror = filepickerErrorCallback;
 
-  //Load the script tag
-  var head = document.getElementsByTagName('head')[0];
-  return head.appendChild(script);
+  // Load the script tag
+  document.getElementsByTagName('head')[0].appendChild(script);
 };
